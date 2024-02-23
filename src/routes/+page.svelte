5cda11d1f6829532fb534b0969e1083955dd1937
deyixtan <deyixtan@gmail.com>
2024-02-23 18:10:16 +0800

    Fix theme-related issues

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -29,10 +29,10 @@
 	}
 </script>
 
-<div role="tablist" class="tabs tabs-lifted bg-neutral">
-	<input type="radio" name="tabList" role="tab" class="tab" aria-label="Chat" />
-	<div role="tabpanel" class="tab-content h-full rounded-box bg-base-100 p-6">
-		<div class="base-300 mt-3 flex-grow overflow-y-auto p-3">
+<div role="tablist" class="tabs-boxed tabs bg-base-100">
+	<input type="radio" name="tabList" role="tab" class="tab" aria-label="Chat" checked />
+	<div role="tabpanel" class="tab-content h-full bg-base-100 p-6">
+		<div class="mt-3 flex-grow overflow-y-auto border-2 bg-base-100 p-3">
 			{#each $messages as { user, text }}
 				<div class="my-2 bg-primary p-4">
 					<strong>{user}</strong>:
@@ -40,35 +40,34 @@
 				</div>
 			{/each}
 		</div>
-		<div class="base-300 mt-3 p-3">
-			<label for="inputPrompt">Prompt:</label>
-			<textarea
-				id="inputPrompt"
-				class="textarea textarea-bordered textarea-primary my-2 w-full"
-				bind:value={prompt}
-			/>
-			<label for="inputImages">Images (optional):</label>
-			<input
-				id="inputImages"
-				type="file"
-				accept="image/*"
-				multiple
-				class="file-input file-input-bordered file-input-primary my-2 w-full"
-				on:change={handleFileUpload}
-			/>
+		<div class="base-100 mt-3 p-3">
+			<label class="label">
+				<span class="label-text mr-3">Prompt</span>
+				<textarea
+					class="textarea textarea-bordered textarea-primary my-2 w-full"
+					bind:value={prompt}
+				/>
+			</label>
+			<label class="label">
+				<span class="label-text mr-3">Images (optional)</span>
+				<input
+					type="file"
+					accept="image/*"
+					multiple
+					class="file-input file-input-bordered file-input-primary my-2 w-full"
+					on:change={handleFileUpload}
+				/>
+			</label>
 			<button class="btn btn-primary my-2 w-full" on:click={handleSend}>Send</button>
 		</div>
 	</div>
 
 	<input type="radio" name="tabList" role="tab" class="tab" aria-label="Settings" />
-	<div role="tabpanel" class="tab-content h-full rounded-box bg-base-100 p-6">
-		<label for="inputKey">API Key:</label>
-		<input
-			id="inputKey"
-			type="text"
-			class="input input-bordered input-primary mt-2 w-full"
-			bind:value={key}
-		/>
+	<div role="tabpanel" class="tab-content h-full bg-base-100 p-6">
+		<label class="label">
+			<span class="label-text">API Key</span>
+			<input type="text" class="input input-bordered input-primary" bind:value={key} />
+		</label>
 		<button class="btn btn-primary my-2 w-full" on:click={handleSave}>Save</button>
 	</div>
 </div>
